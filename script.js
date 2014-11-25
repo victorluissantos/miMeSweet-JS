2fcdee3293b54ccb68cfa1a3b4b1d11630df2d66
Santos L. Victor <victorluissantos@live.com>
2014-11-25 16:29:54 -0200

    up js

--- script.js
+++ script.js
@@ -10,6 +10,38 @@ $(document).ready(function() {
         })
     })
 
+    /*Cronometro*/
+	var segundo = 0+"0";
+	var minuto = 0+"0";
+	var hora = 0+"0";
+	 
+	function tempo(){ 
+	   if (segundo < 59){
+	      segundo++
+	      if(segundo < 10){segundo = "0"+segundo}
+	   }else 
+	      if(segundo == 59 && minuto < 59){
+	         segundo = 0+"0";
+	  minuto++;
+	  if(minuto < 10){minuto = "0"+minuto}
+	      }
+	   if(minuto == 59 && segundo == 59 && hora < 23){
+	      segundo = 0+"0";
+	      minuto = 0+"0";
+	      hora++;
+	      if(hora < 10){hora = "0"+hora}
+	   }else 
+	      if(minuto == 59 && segundo == 59 && hora == 23){
+	         segundo = 0+"0";
+	  minuto = 0+"0";
+	  hora = 0+"0";
+	      }
+	      var html ="";
+	      $('#cronometro').html(html);
+	      $('#cronometro').html("<b>Tempo:</b> "+hora +":"+ minuto +":"+ segundo);
+	}
+
+
  	/*Expressa formulária de level*/
  	/*
  	l = level (1,2,3)
